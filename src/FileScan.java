f3ab87c807d46abba40015381e77080968a8c726
Matt Bennett <144359294+filmteacher@users.noreply.github.com>
2025-08-01 12:23:49 -0400

    First Draft

--- src/FileScan.java
+++ src/FileScan.java
@@ -9,74 +9,72 @@ import javax.swing.JFileChooser;
 /**
  *
  * @author Tom Wulf Tom.Wulf@uc.edu - for original JFileChooserDemo
- * @author Matt Bennett matt.bennett@uc.edu Lab 12
+ * @author Matt Bennett matt.bennett@uc.edu Lab 14
  */
 
 public class FileScan {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //Initialize variables
+        Path target = null;
+        Scanner inFile = null;
+        String[] words;
+        String line;
         int lineCount = 0;
         int wordCount = 0;
         int charCount = 0;
 
-        //Create a java program FileScan.java that uses the JFileChooser dialog to let the user specify a text file from the system.
-        JFileChooser chooser = new JFileChooser();
-        Scanner inFile;
-        String line;
-        Path target = new File(System.getProperty("user.dir")).toPath();
-        //(Code the JFileChooser to open in the src directory of the IntelliJ project.)
-        target = target.resolve("src");
-        chooser.setCurrentDirectory(target.toFile());
-
         try // Code that might trigger the exception goes here
         {
-
-            if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            //The main code will now check to see if there are any cmd line arguments
+            if (args.length > 0) 
             {
-                target = chooser.getSelectedFile().toPath();  // this is a File object, not a String filename
+                //which if present should be the name of a text file in the current directory to scan
+                target = new File(args[0]).toPath();
+            } 
+            else 
+            {
+                //If no argument is present, the program should run as before and launch the JFileChooser to allow the user to interactively pick the file to be scanned.
+                JFileChooser chooser = new JFileChooser();
 
-                inFile = new Scanner(target);
+                target = new File(System.getProperty("user.dir")).toPath();
+                target = target.resolve("src");
+                chooser.setCurrentDirectory(target.toFile());
 
-                while(inFile.hasNextLine()) {
-                    //The program should then open and read the file line by line
-                    line = inFile.nextLine();
-
-                    //echo the lines to the screen
-                    System.out.println(line);
-
-                    //Read each line one at a time keeping track of how many lines you read.
-                    lineCount++;
-
-                    //As you read each line, count how many words there are.
-                    //You can use the split function to return an array of the words, and the length of the array is how many words there are.
-                    String[] words = line.split(" ");
-                    wordCount += words.length;
-
-                    //Finally, as you read each line, use the String length property to count how many characters are in the line and keep track of the total length of the file.
-                    charCount += line.length();
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+                {
+                    target = chooser.getSelectedFile().toPath();  // this is a File object, not a String filename
                 }
-
-                //Then, the program should print a summary report of the file to the screen.
-                //The name of the file the user chose to process
-                System.out.printf("File: %s\n", target.getFileName());
-                //Number of lines in the file
-                System.out.println( "Number of lines: " + lineCount);
-                //Number of words in the file
-                System.out.println( "Number of words: " + wordCount);
-                //Number of characters in the file
-                System.out.println( "Number of characters: " + charCount);
-
-                inFile.close();
+                // User did not pick a file, closed the chooser
+                else
+                {
+                    System.out.println("Sorry, you must select a file! Terminating!");
+                    System.exit(0);
+                }
             }
-            // User did not pick a file, closed the chooser
-            else
+                
+            inFile = new Scanner(target);
+
+            while (inFile.hasNextLine()) 
             {
-                System.out.println("Sorry, you must select a file! Terminating!");
-                System.exit(0);
+                //The program should then open and read the file line by line
+                line = inFile.nextLine();
+
+                //echo the lines to the screen
+                System.out.println(line);
+
+                //Read each line one at a time keeping track of how many lines you read.
+                lineCount++;
+
+                //As you read each line, count how many words there are.
+                //You can use the split function to return an array of the words, and the length of the array is how many words there are.
+                words = line.split(" ");
+                wordCount += words.length;
+
+                //Finally, as you read each line, use the String length property to count how many characters are in the line and keep track of the total length of the file.
+                charCount += line.length();
             }
         }
         catch (FileNotFoundException e) // code to handle this exception
@@ -89,17 +87,20 @@ public class FileScan {
             System.out.println("IOException Error");
             e.printStackTrace();
         }
+
+        //Then, the program should print a summary report of the file to the screen.
+        //The name of the file the user chose to process
+        System.out.printf("File: %s\n", target.getFileName());
+
+        //Number of lines in the file
+        System.out.println("Number of lines: " + lineCount);
+
+        //Number of words in the file
+        System.out.println("Number of words: " + wordCount);
+
+        //Number of characters in the file
+        System.out.println("Number of characters: " + charCount);
+
+        inFile.close();
     }
-
 }
-
-//Screenshots
-//Paste a screenshot here that shows the JFileChooser running.
-//Paste 2 screenshots here that show your output stats for 2 successful program runs on different files.
-//
-//Criteria:
-//Program uses JFileChooser to allow the user to pick a file.
-//It reads the file, echoing it to the screen and then displays
-//the file info number of lines,
-//number of words,
-//and number of characters
